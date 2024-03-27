dc92ee1cc050a2c08881001e274af50ecc323e0f
allan b <https://github.com/allankobejordan/udemy_react.git>
2024-03-27 11:48:19 -0300

    001

--- app/Database/Migrations/2024-03-25-222951_Users.php
+++ app/Database/Migrations/2024-03-25-222951_Users.php
@@ -8,11 +8,51 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '200',
+            ],
+
+            'role' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50',
+                'null' => true
+            ],
+
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
--- app/Database/Migrations/2024-03-25-223013_UserComplaints.php
+++ app/Database/Migrations/2024-03-25-223013_UserComplaints.php
@@ -8,11 +8,48 @@ class UserComplaints extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addFiled([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+
+            'complaint_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimary('id');
+        $this->forge->createTable('user_complaints');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('user_complaints');
     }
 }
